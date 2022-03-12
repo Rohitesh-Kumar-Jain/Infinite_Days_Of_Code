e6f71b8e4981eb7344aa6b49bb11d3c7c4bb3e6b
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2022-03-12 19:36:00 +0530

    Time: 1 ms (41.95%), Space: 45.8 MB (21.28%) - LeetHub

---
+++ 138-copy-list-with-random-pointer/138-copy-list-with-random-pointer.java
@@ -0,0 +1,63 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    public Node copyRandomList(Node head) {
+        
+        if (head == null) return null;
+        
+        Map<Node, Node> map = new HashMap<>();
+
+        Node head2 = new Node(-1);
+        Node prev = head2;
+        Node cur = head;
+        
+        while(cur != null) {
+            if (!map.containsKey(cur)) {
+                Node temp = new Node(cur.val);
+                map.put(cur, temp);
+            }
+            
+            Node temp = map.get(cur);
+            prev.next = temp;
+            
+            if (!map.containsKey(cur.random)) {
+                Node tempRand = null;
+                if (cur.random != null) tempRand = new Node(cur.random.val);
+                map.put(cur.random, tempRand);
+            }
+            
+            temp.random = map.get(cur.random);
+            cur = cur.next;
+            prev = prev.next;
+        }
+        
+        Node travel = head2;
+        
+        // while(travel != null) {
+        //     System.out.print(travel.val + " ");
+        //     if (travel.random == null) System.out.print("NULL" + " ");
+        //     else {
+        //         System.out.print(travel.random.val + " ");
+        //     }
+        //     System.out.println();
+        //     travel = travel.next;
+        // }
+        
+        head2 = head2.next;
+        
+        return head2;
+    }
+}
