a32dcecd8114d1b64d93c2febe28649ed3dbd189
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2022-03-15 18:02:45 +0530

    Time: 30 ms (78.74%), Space: 53.9 MB (56.32%) - LeetHub

---
+++ 1654-minimum-jumps-to-reach-home/1654-minimum-jumps-to-reach-home.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumJumps(int[] forbidden, int a, int b, int x) {
+        int ans = 0;
+        Set<Integer> no = new HashSet<>();
+        for (int i : forbidden) no.add(i);
+        Queue<int[]> bfs = new LinkedList<>();
+        bfs.add(new int[]{0 , 0});
+        
+        while(bfs.size() > 0) {
+            int k = bfs.size();
+            
+            for (int t = 0; t < k; t++) {
+                int[] arr = bfs.poll();
+                int cur = arr[0], backs = arr[1];
+                
+                if (no.contains(cur) || cur < 0 || cur > 8000) continue;
+                if (cur == x) return ans;
+                no.add(cur);
+                
+                if (backs == 0) bfs.add(new int[]{cur - b, 1});
+                bfs.add(new int[]{cur + a, 0});
+            }
+            
+            ans++;
+        } 
+        
+        return -1;
+    }
+}
