adab37c8118160ab5705e1683f7b954a5a739526
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2022-03-19 12:57:04 +0530

    Time: 753 ms (17.45%), Space: 119.4 MB (5.24%) - LeetHub

---
+++ 127-word-ladder/127-word-ladder.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        Set<String> seen = new HashSet<>();
+        Set<String> valid = new HashSet<>(wordList);
+        int ans = 0;
+        
+        Queue<String> bfs = new LinkedList<>();
+        bfs.add(beginWord);
+        
+        while(bfs.size() > 0) {
+            int k = bfs.size();
+            
+            System.out.println(bfs);
+            
+            for (int t = 0; t < k; t++) {
+                
+                String cur = bfs.poll();
+                if (seen.contains(cur)) continue;
+                seen.add(cur);
+                
+                if (cur.equals(endWord)) return ans + 1;
+                
+                for (int i = 0; i < cur.length(); i++) {
+                    for (char ch = 'a'; ch <= 'z'; ch++) {
+                        String st = "";
+                        if (i > 0) st = st + cur.substring(0, i); 
+                        st = st + ch + cur.substring(i+1);
+                        
+                        // System.out.print(st + " ");
+                        if (!seen.contains(st) && valid.contains(st)) bfs.add(st);
+                    }
+                    // System.out.println();
+                }
+            }
+            ans++;
+        }
+        
+        // System.out.println(ans + " " + valid);
+        
+        return 0;
+    }
+}
