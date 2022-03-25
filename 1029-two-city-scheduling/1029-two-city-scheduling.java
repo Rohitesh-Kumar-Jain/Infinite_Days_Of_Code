90182c964022fffb007777cc68c8acb5936f9dff
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2022-03-25 13:03:43 +0530

    Time: 2 ms (78.58%), Space: 43.3 MB (5.24%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -2,30 +2,30 @@ class Solution {
     int n;
     int max = Integer.MAX_VALUE;
     int[][] costs;
-    Integer[][][] memo;
+    Integer[][] memo;
     public int twoCitySchedCost(int[][] costs) {
         n = costs.length/2;
         this.costs = costs;
         
-        memo = new Integer[2*n + 1][n + 1][n + 1];
+        memo = new Integer[2*n + 1][n + 1];
         
-        return dp(0, n, n);
+        return dp(0, n);
     }
     
-    private int dp(int i, int a, int b) {
-        if (i >= 2* n && a == b) {
+    private int dp(int i, int a) {
+        if (i >= 2* n && a == 0 && 2*n - i - a == 0) {
             return 0;
         }
-        if (a < 0 || b < 0) return max;
+        if (a < 0 || 2*n - i - a < 0) return max;
         
-        if (memo[i][a][b] != null) return memo[i][a][b];
+        if (memo[i][a] != null) return memo[i][a];
         
-        int op1 = dp(i + 1, a - 1, b);
+        int op1 = dp(i + 1, a - 1);
         if (op1 != max) op1 += costs[i][0];
         
-        int op2 = dp(i + 1, a, b - 1);
+        int op2 = dp(i + 1, a);
         if (op2 != max) op2 += costs[i][1];
         
-        return memo[i][a][b] = Math.min(op1, op2);
+        return memo[i][a] = Math.min(op1, op2);
     }
 }
