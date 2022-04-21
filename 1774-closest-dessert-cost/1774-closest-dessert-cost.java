63c15bfdbf2f8231e2e4817ee80361a493f7b52d
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2022-04-21 16:03:57 +0530

    Time: 40 ms (23.71%), Space: 42.3 MB (19.69%) - LeetHub

---
+++ 1774-closest-dessert-cost/1774-closest-dessert-cost.java
@@ -0,0 +1,56 @@
+class Solution {
+    
+    int ans;
+    int m;
+    int[] toppingCosts;
+    int target;
+    
+    public int closestCost(int[] baseCosts, int[] toppingCosts, int target) {
+        this.m = toppingCosts.length;
+        this.toppingCosts = toppingCosts;
+        this.target = target;
+        this.ans = baseCosts[0];
+        
+        for (int i : baseCosts) {
+            
+            int val = dp(i, 0);
+            if (Math.abs(target - ans) > Math.abs(target - val)) {
+                ans = val;
+                
+            } else if (Math.abs(target - ans) == Math.abs(target - val)) {
+                ans = Math.min(ans, val);
+            }
+            
+            System.out.println(val + " " + ans);
+        }
+        
+        return ans;
+    }
+    
+    private int dp(int cost, int i) {
+        if (cost == target) return cost;
+        
+        // if (cost - target > ans) {
+        //     return Integer.MAX_VALUE;
+        // }
+        
+        if (m == i) {
+            return cost;
+        }
+        
+        // doNothing
+        int op1 = dp(cost, i + 1);
+        // buy topping once
+        int op2 = dp(cost + toppingCosts[i], i + 1);
+        // buy topping twice
+        int op3 = dp(cost + 2*toppingCosts[i], i + 1);
+        
+        if (Math.abs(op1 - target) > Math.abs(op2 - target)) op1 = op2;
+        else if (Math.abs(op1 - target) == Math.abs(op2 - target)) op1 = Math.min(op1, op2);
+        
+        if (Math.abs(op1 - target) > Math.abs(op3 - target)) op1 = op3;
+        else if (Math.abs(op1 - target) == Math.abs(op3 - target)) op1 = Math.min(op1, op3);
+        
+        return op1;
+    }
+}
