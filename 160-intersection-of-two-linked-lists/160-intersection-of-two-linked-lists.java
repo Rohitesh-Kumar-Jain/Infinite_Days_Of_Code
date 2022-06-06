5622c7758e2c612214bc450e345552cc0ff09fc5
Rohitesh Kumar Jain <thekumarjain@gmail.com>
2022-06-06 10:26:50 +0530

    Time: 1 ms (99.17%), Space: 55.9 MB (13.26%) - LeetHub

--- 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
+++ 160-intersection-of-two-linked-lists/160-intersection-of-two-linked-lists.java
@@ -11,15 +11,39 @@
  */
 public class Solution {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> set = new HashSet<>();
+        int d = 0, e = 0, f;
         
-        while(headA != null) {
-            set.add(headA);
-            headA = headA.next;
+        ListNode cur = headA;
+        while(cur != null) {
+            cur = cur.next; d++;
         }
         
-        while(headB != null) {
-            if (set.contains(headB)) return headB;
+        cur = headB;
+        while(cur != null) {
+            cur = cur.next; e++;
+        }
+        
+        f = d - e;
+        
+        if (f > 0) {
+            for (int i = f; i > 0; i--) {
+                if (headA == null) break;
+                headA = headA.next;
+            }
+            
+        } else {
+            f = Math.abs(f);
+            for (int i = f; i > 0; i--) {
+                if (headB == null) break;
+                headB = headB.next;
+            }
+        }
+        
+        // System.out.println(d + " " + e + " " + f);
+        
+        while(headA != null && headB != null) {
+            if (headA == headB) return headA;
+            headA = headA.next;
             headB = headB.next;
         }
         
